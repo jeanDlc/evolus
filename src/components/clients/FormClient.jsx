58cdd11be22a2f3f7e07b5a041d014bb43ecc124
Jean Pierre <jeanpierre101298gmail.com>
2021-07-01 15:07:52 -0500

    useForm (validación) en FormClient implementado

--- src/components/clients/FormClient.jsx
+++ src/components/clients/FormClient.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardContent, Container, FormControl, Grid, Input, InputAdornment, InputLabel, Typography } from '@material-ui/core';
+import { Button, Card, CardContent, Container, FormControl, Grid, Input, InputAdornment, InputLabel, Typography,FormHelperText } from '@material-ui/core';
 import PhoneIcon from '@material-ui/icons/Phone';
 import EmailIcon from '@material-ui/icons/Email';
 import HomeIcon from '@material-ui/icons/Home';
@@ -10,41 +10,20 @@ import {toast} from 'react-toastify';
 import {useParams} from 'react-router-dom';
 import { useEffect } from 'react';
 import EditIcon from '@material-ui/icons/Edit';
+import useForm from '../../lib/hooks/useForm';
+import { validateClient } from '../../lib/validation/forms/client';
 const FormNewClient = () => {
     const params=useParams();
     const redirectTo=useRedirecTo();
+    //estado inicial del cliente
     const initialState={
         nombre:"", apellidos:"",
         num_telefonico:"", email:"",
         dni:"", ruc:"",
         direccion:""
     }
-    const [client,setClient]=useState(initialState);
-    useEffect(()=>{
-        let isMounted=true;
-        if(params.id){
-            getClientById(params.id)
-            .then(res=>{
-                if(isMounted) setClient(res)
-            })
-            .catch(error=>{
-                console.log(error.response);
-                redirectTo('/clientes');
-                toast.error(error.response?.data?.error || 'Error');
-            })
-        }else{
-            if(isMounted) setClient(initialState)
-        }
-        return ()=>isMounted=false;
-    },[params.id]);
-    const handleChange=e=>{
-        setClient({
-            ...client,
-            [e.target.name]:e.target.value
-        })
-    }
-    const handleSubmit=async e=>{
-        e.preventDefault();
+    //o crea o edita un cliente
+    const handleClient=async()=>{
         try {
             let res;
             if(params.id){
@@ -64,8 +43,28 @@ const FormNewClient = () => {
                 toast.error(error.response?.data?.error || 'Ocurrió un error');
             }
         }
-        
     }
+    
+    const {handleChange, handleSubmit,errors,fields:client={}, setFields:setClient}=useForm(initialState,validateClient, handleClient);
+
+    useEffect(()=>{
+        let isMounted=true;
+        if(params.id){
+            //si estamos en edición-->rellenar el formulario
+            getClientById(params.id)
+            .then(res=>{
+                if(isMounted) setClient(res)
+            })
+            .catch(error=>{
+                console.log(error.response);
+                redirectTo('/clientes');
+                toast.error(error.response?.data?.error || 'Error');
+            })
+        }else{
+            if(isMounted) setClient(initialState)
+        }
+        return ()=>isMounted=false;
+    },[params.id]);
     return ( 
         <Container maxWidth='md' style={{marginTop:40}} >
             <Card>
@@ -81,25 +80,37 @@ const FormNewClient = () => {
                     </Typography>
                     <Grid container spacing={3} >
                         <Grid item xs={12} md={6}  >
-                            <FormControl color='secondary'  margin='normal' fullWidth={true}>
+                            <FormControl color='secondary'  
+                                error={errors.nombre}
+                                margin='normal' fullWidth={true}>
                                 <InputLabel htmlFor='nombre' >Nombre de cliente</InputLabel>
                                 <Input id='nombre' name='nombre' 
                                     value={client.nombre} 
                                     onChange={handleChange}
                                 />
+                                {errors.nombre && (
+                                    <FormHelperText>{errors.nombre} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <FormControl color='secondary'  margin='normal' fullWidth={true} >
+                            <FormControl color='secondary'  
+                                error={errors.apellidos}
+                                margin='normal' fullWidth={true} >
                                 <InputLabel htmlFor="apellidos">Apellidos del cliente</InputLabel>
                                 <Input id="apellidos" name='apellidos' type='text' 
                                     value={client.apellidos} 
                                     onChange={handleChange}
                                 />
+                                {errors.apellidos && (
+                                    <FormHelperText>{errors.apellidos} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <FormControl color='secondary'  margin='normal' fullWidth={true} >
+                            <FormControl color='secondary'  
+                                error={errors.num_telefonico}
+                                margin='normal' fullWidth={true} >
                                 <InputLabel htmlFor="num_telefonico">Número telefónico</InputLabel>
                                 <Input startAdornment={
                                     <InputAdornment>
@@ -109,10 +120,15 @@ const FormNewClient = () => {
                                     value={client.num_telefonico} 
                                     onChange={handleChange}
                                 />
+                                {errors.num_telefonico && (
+                                    <FormHelperText>{errors.num_telefonico} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <FormControl color='secondary'  margin='normal' fullWidth={true} >
+                            <FormControl color='secondary'  
+                                error={errors.email}
+                                margin='normal' fullWidth={true} >
                                 <InputLabel htmlFor="email">Correó electrónico (email)</InputLabel>
                                 <Input startAdornment={
                                     <InputAdornment>
@@ -122,28 +138,43 @@ const FormNewClient = () => {
                                     value={client.email} 
                                     onChange={handleChange}
                                 />
+                                {errors.email && (
+                                    <FormHelperText>{errors.email} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <FormControl color='secondary'  margin='normal' fullWidth={true} >
+                            <FormControl color='secondary'  
+                                error={errors.dni}
+                                margin='normal' fullWidth={true} >
                                 <InputLabel htmlFor="dni">DNI</InputLabel>
                                 <Input id="dni" name='dni' type='text' 
                                     value={client.dni} 
                                     onChange={handleChange}
                                 />
+                                {errors.dni && (
+                                    <FormHelperText>{errors.dni} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <FormControl color='secondary'  margin='normal' fullWidth={true} >
+                            <FormControl color='secondary'  
+                                error={errors.ruc}
+                                margin='normal' fullWidth={true} >
                                 <InputLabel htmlFor="ruc">RUC</InputLabel>
                                 <Input id="ruc" name='ruc' type='text' 
                                     value={client.ruc} 
                                     onChange={handleChange}
                                 />
+                                {errors.ruc && (
+                                    <FormHelperText>{errors.ruc} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <FormControl color='secondary'  margin='normal' fullWidth={true} >
+                            <FormControl color='secondary'  
+                                error={errors.direccion}
+                                margin='normal' fullWidth={true} >
                                 <InputLabel htmlFor="direccion">Dirección del cliente</InputLabel>
                                 <Input startAdornment={
                                     <InputAdornment>
@@ -153,6 +184,9 @@ const FormNewClient = () => {
                                     value={client.direccion} 
                                     onChange={handleChange}
                                 />
+                                {errors.direccion && (
+                                    <FormHelperText>{errors.direccion} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6} >
--- src/lib/hooks/useForm.jsx
+++ src/lib/hooks/useForm.jsx
@@ -24,7 +24,7 @@ const useForm = (initialState, validate, fn) => {
         setErrors(validationErrors);
         setSubmit(true);
     }
-    return {errors, handleChange,handleSubmit, fields}
+    return {errors, handleChange,handleSubmit, fields,setFields}
 }
  
 export default useForm;
