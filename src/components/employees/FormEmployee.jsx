40be94331df036fcc1591e640849dee316a1de6b
Jean Pierre <jeanpierre101298gmail.com>
2021-07-01 15:37:57 -0500

    useForm funciona perfectamente en formEmployee

--- src/components/employees/FormEmployee.jsx
+++ src/components/employees/FormEmployee.jsx
@@ -5,13 +5,15 @@ import HomeIcon from '@material-ui/icons/Home';
 import LockIcon from '@material-ui/icons/Lock';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import {useParams} from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
+import React, {  useEffect } from 'react';
 import {getEmployeeById} from '../../lib/services/employees';
 import {toast} from 'react-toastify';
 import useRedirecTo from '../../lib/hooks/useRedirecTo';
 import {newEmployee, updateEmployee} from '../../lib/services/employees';
 import useRoles from '../../lib/hooks/useRoles';
 import EditIcon from '@material-ui/icons/Edit';
+import useForm from '../../lib/hooks/useForm';
+import { validateClient } from '../../lib/validation/forms/employee';
 const FormEmployee = () => {
     const roles=useRoles();
     const redirectTo=useRedirecTo();
@@ -23,7 +25,28 @@ const FormEmployee = () => {
         direccion:"", pass:"",
         RolId:0, confirmar:""
     }
-    const [employee, setEmployee]=useState(initialState);
+    const handleEmployee=async()=>{
+        try {
+            let res;
+            if(params.id){
+                //update
+                res=await updateEmployee(params.id, employee);
+            }else{
+                //create new employee
+                res=await newEmployee(employee);
+            }
+            toast.success(res.msg || 'Éxito');
+            redirectTo('/empleados');
+        } catch (error) {
+            if(error.response?.data?.errores){
+                const arrayErrors=error.response.data.errores;
+                arrayErrors.forEach(err=>toast.error(err.msg));
+            }else{
+                toast.error(error.response?.data?.error || 'Ocurrió un error');
+            }
+        }
+    }
+    const {errors,fields:employee={},setFields:setEmployee, handleSubmit, handleChange}=useForm(initialState,validateClient,handleEmployee);
     useEffect(()=>{
         let isMounted=true;
         if(params.id){
@@ -46,35 +69,8 @@ const FormEmployee = () => {
             if(isMounted) setEmployee(initialState)
         }
         return ()=>isMounted=false;
-    },[params.id])
-    const handleChange=e=>{
-        setEmployee({
-            ...employee,
-            [e.target.name]:e.target.value
-        })
-    }
-    const handleSubmit=async e=>{
-        e.preventDefault();
-        try {
-            let res;
-            if(params.id){
-                //update
-                res=await updateEmployee(params.id, employee);
-            }else{
-                //create new employee
-                res=await newEmployee(employee);
-            }
-            toast.success(res.msg || 'Éxito');
-            redirectTo('/empleados');
-        } catch (error) {
-            if(error.response?.data?.errores){
-                const arrayErrors=error.response.data.errores;
-                arrayErrors.forEach(err=>toast.error(err.msg));
-            }else{
-                toast.error(error.response?.data?.error || 'Ocurrió un error');
-            }
-        }
-    }
+    },[params.id]);
+
     return ( 
         <Container maxWidth='md' style={{marginTop:50}}>
             <Card>
@@ -91,25 +87,37 @@ const FormEmployee = () => {
                     
                     <Grid container spacing={3} >
                         <Grid item xs={12} md={6}>
-                            <FormControl color='secondary'  margin='normal' fullWidth={true} >
+                            <FormControl color='secondary'  
+                                error={errors.nombre}
+                                margin='normal' fullWidth={true} >
                                 <InputLabel htmlFor="nombre">Nombre del empleado</InputLabel>
                                 <Input id="nombre" name='nombre' type='text' 
                                     onChange={handleChange}
                                     value={employee.nombre}
                                 />
+                                {errors.nombre && (
+                                    <FormHelperText>{errors.nombre} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <FormControl color='secondary'  margin='normal' fullWidth={true} >
+                            <FormControl color='secondary'  
+                                error={errors.apellidos}
+                                margin='normal' fullWidth={true} >
                                 <InputLabel htmlFor="apellidos">Apellidos del empleado</InputLabel>
                                 <Input id="apellidos" name='apellidos' type='text' 
                                     onChange={handleChange}
                                     value={employee.apellidos}
                                 />
+                                {errors.apellidos && (
+                                    <FormHelperText>{errors.apellidos} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <FormControl color='secondary'  margin='normal' fullWidth={true} >
+                            <FormControl color='secondary'  
+                                error={errors.num_telefonico}
+                                margin='normal' fullWidth={true} >
                                 <InputLabel htmlFor="num_telefonico">Número telefónico</InputLabel>
                                 <Input startAdornment={
                                     <InputAdornment>
@@ -119,11 +127,16 @@ const FormEmployee = () => {
                                     value={employee.num_telefonico}
                                     onChange={handleChange}
                                 />
+                                {errors.num_telefonico && (
+                                    <FormHelperText>{errors.num_telefonico} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         {!params.id && (
                             <Grid item xs={12} md={6}>
-                                <FormControl color='secondary'  margin='normal' fullWidth={true} >
+                                <FormControl color='secondary'  
+                                    error={errors.email}
+                                    margin='normal' fullWidth={true} >
                                     <InputLabel htmlFor="email">Correó electrónico (email)</InputLabel>
                                     <Input startAdornment={
                                         <InputAdornment>
@@ -133,29 +146,44 @@ const FormEmployee = () => {
                                         onChange={handleChange}
                                         value={employee.email}
                                     />
+                                    {errors.email && (
+                                        <FormHelperText>{errors.email} </FormHelperText>
+                                    )}
                                 </FormControl>
                             </Grid>
                         )}
                         <Grid item xs={12} md={6}>
-                            <FormControl color='secondary'  margin='normal' fullWidth={true} >
+                            <FormControl color='secondary'  
+                                error={errors.dni}
+                                margin='normal' fullWidth={true} >
                                 <InputLabel htmlFor="dni">DNI</InputLabel>
                                 <Input id="dni" name='dni' type='text' 
                                     onChange={handleChange}
                                     value={employee.dni}
                                 />
+                                {errors.dni && (
+                                    <FormHelperText>{errors.dni} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <FormControl color='secondary'  margin='normal' fullWidth={true} >
+                            <FormControl color='secondary'  
+                                error={errors.ruc}
+                                margin='normal' fullWidth={true} >
                                 <InputLabel htmlFor="ruc">RUC</InputLabel>
                                 <Input id="ruc" name='ruc' type='text' 
                                     onChange={handleChange}
                                     value={employee.ruc}
                                 />
+                                {errors.ruc && (
+                                    <FormHelperText>{errors.ruc} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <FormControl color='secondary'  margin='normal' fullWidth={true} >
+                            <FormControl color='secondary'  
+                                error={errors.direccion}
+                                margin='normal' fullWidth={true} >
                                 <InputLabel htmlFor="direccion">Dirección del empleado</InputLabel>
                                 <Input startAdornment={
                                     <InputAdornment>
@@ -165,10 +193,15 @@ const FormEmployee = () => {
                                     onChange={handleChange}
                                     value={employee.direccion}
                                 />
+                                {errors.direccion && (
+                                    <FormHelperText>{errors.direccion} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <FormControl color='secondary'  margin='normal' fullWidth={true} >
+                            <FormControl color='secondary'  
+                                error={errors.RolId}
+                                margin='normal' fullWidth={true} >
                                 <InputLabel id="label-rol" htmlFor="rol">Cargo/Rol</InputLabel>
                                 <Select
                                     labelId="label-rol"
@@ -182,10 +215,15 @@ const FormEmployee = () => {
                                         <MenuItem key={rol.id} value={rol.id} >{rol.nombre} </MenuItem>    
                                     ))}
                                 </Select>
+                                {errors.RolId && (
+                                    <FormHelperText>{errors.RolId} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <FormControl color='secondary'  margin='normal' fullWidth={true} >
+                            <FormControl color='secondary'  
+                                error={errors.pass}
+                                margin='normal' fullWidth={true} >
                                 <InputLabel htmlFor="pass">Contraseña</InputLabel>
                                 <Input startAdornment={
                                     <InputAdornment>
@@ -196,7 +234,9 @@ const FormEmployee = () => {
                                     onChange={handleChange}
                                     value={employee.pass}
                                 />
-                                <FormHelperText id="helper-pass" >Contraseña para la cuenta de usuario</FormHelperText>
+                                {errors.pass && (
+                                    <FormHelperText>{errors.pass} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6}>
@@ -215,7 +255,9 @@ const FormEmployee = () => {
                                     onChange={handleChange}
                                     value={employee.confirmar}
                                 />
-                                <FormHelperText id="helper-confirmar" >Las contraseñas deben coincidir</FormHelperText>
+                                {errors.confirmar && (
+                                    <FormHelperText>{errors.confirmar} </FormHelperText>
+                                )}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} md={6}>
