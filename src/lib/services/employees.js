289cf0d5377bb5442a7e28a22d85a821d9e03281
Jean Pierre <jeanpierre101298gmail.com>
2021-07-14 14:18:17 -0500

    axiosClient para peticiones en employees.js

--- src/lib/services/employees.js
+++ src/lib/services/employees.js
@@ -1,23 +1,28 @@
-import axios from 'axios';
-const url=`${process.env.REACT_APP_API}/empleados`;
+import axiosClient from "../../config/axios";
+import tokenAuth from "../../config/tokenAuth";
 
-export const getEmployees=async()=>{
-    const res= await axios(url);
-    return res.data;
-}
-export const getEmployeeById=async(id)=>{
-    const res=await axios(`${url}/${id}`);
-    return res.data;
-}
-export const newEmployee=async(employee)=>{
-    const res=await axios.post(url, employee);
-    return res.data;
-}
-export const updateEmployee=async(id, employee)=>{
-    const res=await axios.put(`${url}/${id}`, employee);
-    return res.data;
-}
-export const deleteEmployee=async(id)=>{
-    const res=await axios.delete(`${url}/${id}`);
-    return res.data;
-}
+export const getEmployees = async () => {
+  tokenAuth();
+  const res = await axiosClient("/empleados");
+  return res.data;
+};
+export const getEmployeeById = async (id) => {
+  tokenAuth();
+  const res = await axiosClient(`/empleados/${id}`);
+  return res.data;
+};
+export const newEmployee = async (employee) => {
+  tokenAuth();
+  const res = await axiosClient.post("/empleados", employee);
+  return res.data;
+};
+export const updateEmployee = async (id, employee) => {
+  tokenAuth();
+  const res = await axiosClient.put(`/empleados/${id}`, employee);
+  return res.data;
+};
+export const deleteEmployee = async (id) => {
+  tokenAuth();
+  const res = await axiosClient.delete(`/empleados/${id}`);
+  return res.data;
+};
