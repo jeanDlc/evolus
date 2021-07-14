82ce86767b5cf34db62a29ca85455867a0344bf8
Jean Pierre <jeanpierre101298gmail.com>
2021-07-14 14:30:39 -0500

    peticiones en project.js con token en header

--- src/lib/services/project.js
+++ src/lib/services/project.js
@@ -1,32 +1,39 @@
-import axios from 'axios';
-const url=`${process.env.REACT_APP_API}/proyectos`
-export const getProjects=async()=>{
-    const res=await axios(url);
-    return res.data;
-}
-export const getProjectById=async(idProject)=>{
-    const res=await axios(`${url}/${idProject}`);
-    return res.data;
-}
-export const getProjectTasks=async(idProject)=>{
-    const res=await axios(`${url}/${idProject}/tareas`);
-    return res.data;
-}
-export const newProject=async(project)=>{
-    const res=await axios.post(url, project);
-    return res.data;
-}
+import axiosClient from "../../config/axios";
+import tokenAuth from "../../config/tokenAuth";
+export const getProjects = async () => {
+  tokenAuth();
+  const res = await axiosClient("/proyectos");
+  return res.data;
+};
+export const getProjectById = async (idProject) => {
+  tokenAuth();
+  const res = await axiosClient(`/proyectos/${idProject}`);
+  return res.data;
+};
+export const getProjectTasks = async (idProject) => {
+  tokenAuth();
+  const res = await axiosClient(`/proyectos/${idProject}/tareas`);
+  return res.data;
+};
+export const newProject = async (project) => {
+  tokenAuth();
+  const res = await axiosClient.post("/proyectos", project);
+  return res.data;
+};
 
-export const updateProject=async(idProject,project)=>{
-    const res=await axios.put(`${url}/${idProject}`, project);
-    return res.data;
-}
-export const deleteProject=async(idProject)=>{
-    const res=await axios.delete(`${url}/${idProject}`);
-    return res.data;
-}
+export const updateProject = async (idProject, project) => {
+  tokenAuth();
+  const res = await axiosClient.put(`/proyectos/${idProject}`, project);
+  return res.data;
+};
+export const deleteProject = async (idProject) => {
+  tokenAuth();
+  const res = await axiosClient.delete(`/proyectos/${idProject}`);
+  return res.data;
+};
 
-export const newProjectTask=async(idProject,task)=>{
-    const res=await axios.post(`${url}/${idProject}/tareas`, task);
-    return res.data;
-}
+export const newProjectTask = async (idProject, task) => {
+  tokenAuth();
+  const res = await axiosClient.post(`/proyectos/${idProject}/tareas`, task);
+  return res.data;
+};
