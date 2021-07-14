273f8de7f31c83628cad0ebaea9aa6fb343e947c
Jean Pierre <jeanpierre101298gmail.com>
2021-07-14 14:36:19 -0500

    peticiones en roles y tasks js  con token en header

--- src/lib/services/rol.js
+++ src/lib/services/rol.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
-
-const url=`${process.env.REACT_APP_API}/roles`;
-export const getRoles=async()=>{
-    const res=await axios(url);
-    return res.data;
-}
+import axiosClient from "../../config/axios";
+import tokenAuth from "../../config/tokenAuth";
+export const getRoles = async () => {
+  tokenAuth();
+  const res = await axiosClient("/roles");
+  return res.data;
+};
--- src/lib/services/task.js
+++ src/lib/services/task.js
@@ -1,14 +1,18 @@
-import axios from 'axios';
-const url=`${process.env.REACT_APP_API}/tareas`;
-export const getTaskById=async(idTask)=>{
-    const res=await axios(`${url}/${idTask}`);
-    return res.data;
-}
-export const updateTask=async(idTask,task)=>{
-    const res=await axios.put(`${url}/${idTask}`,task);
-    return res.data;
-}
-export const deleteTask=async(idTask)=>{
-    const res=await axios.delete(`${url}/${idTask}`);
-    return res.data;
-}
+import axiosClient from "../../config/axios";
+import tokenAuth from "../../config/tokenAuth";
+
+export const getTaskById = async (idTask) => {
+  tokenAuth();
+  const res = await axiosClient(`/tareas/${idTask}`);
+  return res.data;
+};
+export const updateTask = async (idTask, task) => {
+  tokenAuth();
+  const res = await axiosClient.put(`/tareas/${idTask}`, task);
+  return res.data;
+};
+export const deleteTask = async (idTask) => {
+  tokenAuth();
+  const res = await axiosClient.delete(`/tareas/${idTask}`);
+  return res.data;
+};
